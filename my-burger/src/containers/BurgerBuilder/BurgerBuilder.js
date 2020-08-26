0d8684c803cc8f0d9e63dd66c8401032a5121ca9
Ankit Jasuja <ankitjasuja84@gmail.com>
2020-08-26 10:04:56 +0530

    Created BurgerBuilder class component,apply bit styling on layout component

---
+++ my-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component,Fragment } from "react";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Fragment>
+          <div>graphical burger</div>
+          <div>build controllers ingredients</div>
+      </Fragment>
+    )
+  }
+}
+
+export default BurgerBuilder;
