ffed04e6d136cf380282c33d6f6424ecfc4a457e
Ankit Jasuja <ankitjasuja84@gmail.com>
2020-08-28 10:15:29 +0530

    outputting burger ingredients dynamically

--- my-burger/src/components/Burger/Burger.js
+++ my-burger/src/components/Burger/Burger.js
@@ -2,15 +2,27 @@ import React from "react";
 import classes from './Burger.module.css'
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 
-const burger = (props)=>{
- return (
-     <div className={classes.Burger}>
-         <BurgerIngredients type="bread-top"></BurgerIngredients>
-         <BurgerIngredients type="cheese"></BurgerIngredients>
-         <BurgerIngredients type="meat"></BurgerIngredients>
-         <BurgerIngredients type="bread-bottom"></BurgerIngredients>
-     </div>
- )
+const burger = (props) => {
+  const transformedIngredients = Object.keys(props.burgerIngredients).map(
+    (igKey) => {
+      return [...Array(props.burgerIngredients[igKey])].map((_, index) => {
+        return (
+          <BurgerIngredients
+            type={igKey}
+            key={igKey + index}
+          ></BurgerIngredients>
+        );
+      });
+    }
+  );
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredients type="bread-top"></BurgerIngredients>
+      {transformedIngredients}
+      <BurgerIngredients type="bread-bottom"></BurgerIngredients>
+    </div>
+  );
 };
 
 export default burger;
--- my-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,20 @@ import React, { Component,Fragment } from "react";
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+  state={
+    ingredients:{
+      salad:1,
+      cheese:2,
+      bacon:3,
+      meat:1
+    }
+  }
+
+
   render() {
     return (
       <Fragment>
-          <Burger/>
+          <Burger burgerIngredients={this.state.ingredients} />
           <div>build controllers ingredients</div>
       </Fragment>
     )
