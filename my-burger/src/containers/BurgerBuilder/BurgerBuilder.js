25a35b3df7eefb7877ff1d384a1eb412d1a818f8
Ankit Jasuja <ankitjasuja84@gmail.com>
2020-09-03 09:26:35 +0530

    Disabling "Less" button when no ingredient present

--- my-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ my-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -46,12 +46,17 @@ class BurgerBuilder extends Component {
   };
 
   render() {
+    const disabledInfo = { ...this.state.ingredients };
+     for (let key in disabledInfo) {
+       disabledInfo[key] = disabledInfo[key] <= 0;
+     }
     return (
       <Fragment>
         <Burger burgerIngredients={this.state.ingredients} />
         <BurgerControls
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
         ></BurgerControls>
       </Fragment>
     );
