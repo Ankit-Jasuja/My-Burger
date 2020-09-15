ecaf6a9b715901a143102aa30fe1b99f6da1834f
Ankit Jasuja <ankitjasuja84@gmail.com>
2020-09-15 09:24:49 +0530

    removing old interceptors when component unmounts to prevent memory leaks(so that old ones are not maintained in memory)

--- my-burger/src/hoc/withErrorHandler/withErrorHandler.js
+++ my-burger/src/hoc/withErrorHandler/withErrorHandler.js
@@ -13,23 +13,28 @@ const withErrorHandler = (WrappedComponent, axios) => {
     };
 
     componentWillMount() {
-      axios.interceptors.request.use((req) => {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
       });
       axios.interceptors.response.use(
-        (res) => res,
+        (this.resInterceptor = (res) => res),
         (error) => {
-          this.setState({ error : error });
+          this.setState({ error: error });
         }
       );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     render() {
       return (
         <Fragment>
           <Modal show={this.state.error} modalClosed={this.closeModel}>
-            {this.state.error!=null?this.state.error.message:null}
+            {this.state.error != null ? this.state.error.message : null}
           </Modal>
           <WrappedComponent {...this.props}></WrappedComponent>
         </Fragment>
